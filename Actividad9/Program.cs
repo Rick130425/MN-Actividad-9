33f0b696cc43e9c35712461f62586d1ddce5386b
Rick <rick1304.25@gmail.com>
2023-10-14 16:09:02 -0600

    Creación de funciones útiles y utilización (Código terminado)

---
+++ Actividad9/Program.cs
@@ -0,0 +1,70 @@
+﻿using Actividad9;
+
+// Inicialización de datos (entrada y salida)
+var data = new[]
+{
+    new double[]
+    {
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17,
+        18, 19, 20
+    },
+
+    new double[]
+    {
+        -2.4684, 56.8038, 4.8393,
+        13.2878, -0.1931, -19.5771,
+        55.4146, 74.4829, 62.0632,
+        37.3616, 14.1876, 72.9088,
+        81.5205, 120.7811, 156.3607,
+        141.7361, 110.898, 129.7614,
+        192.8516, 253.4778
+    }
+};
+
+// Definición de funciones derivadas
+var derivatives = new[]
+{
+    // x^2
+    x => Math.Pow(x, 2),
+    // sin(x)
+    Math.Sin,
+    // e^(x/10)
+    x => Math.Exp(x / 10)
+};
+
+// Inicialización de la matriz Jacobiana
+var jacobian = new double[20, 3];
+
+// Cálculo de la matriz Jacobiana
+for (var row = 0; row < 20; row++)
+{
+    for (var column = 0; column < 3; column++)
+    {
+        jacobian[row, column] = derivatives[column](data[0][row]);
+    }
+}
+
+// Inicialización de una matriz extendida
+var augMatrix = new double[3, 4];
+
+// Transposición de la matriz Jacobiana
+var tJacobian = Utils.Transpose(jacobian);
+
+// Realización de productos de matrices (producto-punto) y guardado en la matriz extendida
+augMatrix = Utils.CopyTo(
+    Utils.DotProduct(tJacobian, jacobian),
+    augMatrix);
+
+// Realización de producto de matrices (producto-punto) entre transpuesta y datos de salida, 
+// y guardada en la columna 3 de la matriz extendida
+augMatrix = Utils.CopyTo(
+    Utils.DotProduct(tJacobian, 
+        Utils.CopyTo(data[1], new double[data[1].Length, 1])), 
+    augMatrix, startColumn: 3);
+
+// Aplicación de eliminación gaussiana
+augMatrix = Utils.GaussianElimination(augMatrix);
+
+// Impresión de resultados
+Console.WriteLine($"{augMatrix[0,3]:0.####}t^2 + {augMatrix[1,3]:0.####}sin(t) + {augMatrix[2,3]:0.####}e^(t/10)");
---
+++ Actividad9/Utils.cs
@@ -0,0 +1,169 @@
+﻿namespace Actividad9;
+
+public static class Utils
+{
+    /// <summary>
+    /// Copia los valores de una matriz en otra.
+    /// </summary>
+    /// <param name="src">Matriz origen.</param>
+    /// <param name="destination">Matriz destino.</param>
+    /// <param name="startRow">Fila inicial.</param>
+    /// <param name="startColumn">Columna inicial.</param>
+    /// <returns>Matriz destino con los valores copiados.</returns>
+    public static double[,] CopyTo(double[,] src, double[,] destination, int startRow = 0, int startColumn = 0)
+    {
+        // Arreglo de dimensiones
+        var size = new double[] { src.GetLength(0), src.GetLength(1) };
+        for (var row = 0; row < size[0]; row++)
+        {
+            for (var column = 0; column < size[1]; column++)
+            {
+                // Se copian los valores del origen al destino, empezando desde las coordenadas dadas
+                destination[row + startRow, column + startColumn] = src[row, column];
+            }
+        }
+
+        // Se regresa la matriz destino con los valores copiados
+        return destination;
+    }
+
+    /// <summary>
+    /// Copia los valores de un arreglo a una matriz dada.
+    /// </summary>
+    /// <param name="array">Arreglo origen.</param>
+    /// <param name="matrix">Matriz destino.</param>
+    /// <param name="startRow">Fila inicial</param>
+    /// <param name="startColumn">Columna inicial</param>
+    /// <returns>Matriz destino con los valores copiados.</returns>
+    public static double[,] CopyTo(double[] array, double[,] matrix, int startRow = 0, int startColumn = 0)
+    {
+        for (var row = 0; row < array.Length; row++)
+        {
+            // Se copian los valores del origen al destino, empezando desde las coordenadas dadas
+            matrix[row + startRow, startColumn] = array[row];
+        }
+
+        // Retorno de la matriz destino con los valores copiados
+        return matrix;
+    }
+
+    /// <summary>
+    /// Realiza una eliminación gaussiana a la matriz introducida.
+    /// </summary>
+    /// <param name="augmentedMatrix">Matriz extendida.</param>
+    /// <returns>Matriz resultante de la eliminación gaussiana.</returns>
+    public static double[,] GaussianElimination(double[,] augmentedMatrix)
+    {
+        // Arreglo de dimensiones
+        var size = new[] { augmentedMatrix.GetLength(0), augmentedMatrix.GetLength(1) };
+        for (var pivotIndex = 0; pivotIndex < size[0]; pivotIndex++)
+        {
+            // Guardado del pivote
+            var pivot = augmentedMatrix[pivotIndex, pivotIndex];
+            for (var row = 0; row < size[0]; row++)
+            {
+                // Fila temporal (iniciando desde la fila del pivote)
+                var tempRow = (pivotIndex + row) % size[0];
+                // Si la fila es 0
+                if (row == 0)
+                {
+                    for (var column = 0; column < size[1]; column++)
+                    {
+                        // División de la fila del pivote por el pivote
+                        augmentedMatrix[tempRow, column] /= pivot;
+                    }
+                }
+                // Si no
+                else
+                {
+                    // Guardado del factor (valor de la columna del pivote en la fila)
+                    var factor = augmentedMatrix[tempRow, pivotIndex];
+                    for (var column = 0; column < size[1]; column++)
+                    {
+                        // Resta de los valores de la fila del pivote, multiplicados por el factor, a la fila actuál
+                        augmentedMatrix[tempRow, column] -= augmentedMatrix[pivotIndex, column] * factor;
+                    }
+                }
+            }
+        }
+        // Retorno de la matriz extendida resultante
+        return augmentedMatrix;
+    }
+    
+    /// <summary>
+    /// Transpone una matriz.
+    /// </summary>
+    /// <param name="matrix">Matriz.</param>
+    /// <returns>Transpuesta de la matriz.</returns>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        // Arreglo de dimensiones
+        var size = new int[] { matrix.GetLength(0), matrix.GetLength(1) };
+        // Declaración de matriz transpuesta
+        var transposedMatrix = new double[size[1], size[0]];
+        for (var i = 0; i < size[0]; i++)
+        {
+            for (var j = 0; j < size[1]; j++)
+            {
+                // Se invierten las coordenadas del valor y se guardan en la matriz transpuesta
+                transposedMatrix[j, i] = matrix[i, j];
+            }
+        }
+        // Retorno de la matriz transpuesta
+        return transposedMatrix;
+    }
+    
+    /// <summary>
+    /// Multiplica la matriz por un coeficiente dado.
+    /// </summary>
+    /// <param name="coefficient">Coeficiente.</param>
+    /// <param name="matrix">Matriz.</param>
+    /// <returns>Matriz resultante de la multiplicación.</returns>
+    public static double[,] MultMatrix(double coefficient, double[,] matrix)
+    {
+        // Arreglo de dimensiones
+        var size = new[] { matrix.GetLength(0), matrix.GetLength(1) };
+        for (var row = 0; row < size[0]; row++)
+        {
+            for (var column = 0; column < size[1]; column++)
+            {
+                // Multiplicación de valores por el coeficiente
+                matrix[row, column] *= coefficient;
+            }
+        }
+        // Retorno de la matriz multiplicada.
+        return matrix;
+    }
+    
+    /// <summary>
+    /// Realiza el producto punto de dos matrices.
+    /// </summary>
+    /// <param name="matrixOne">Primera matriz.</param>
+    /// <param name="matrixTwo">Segunda matriz.</param>
+    /// <returns>Resultado del producto punto.</returns>
+    public static double[,] DotProduct(double[,] matrixOne, double[,] matrixTwo)
+    {
+        // Declaración de la matriz resultante
+        var dotProduct = new double[matrixOne.GetLength(0), matrixTwo.GetLength(1)];
+        // Arreglo de dimensiones
+        var measures = new double[]
+        {
+            dotProduct.GetLength(0),
+            dotProduct.GetLength(1),
+            matrixOne.GetLength(1)
+        };
+        for (var row = 0; row < measures[0]; row++)
+        {
+            for (var column = 0; column < measures[1]; column++)
+            {
+                for (var i = 0; i < measures[2]; i++)
+                {   
+                    // Sumatoria del producto de valores de matrices
+                    dotProduct[row, column] += matrixOne[row, i] * matrixTwo[i, column];
+                }
+            }
+        }
+        // Retorno de la matriz resultante
+        return dotProduct;
+    }
+}
